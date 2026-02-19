ab3585d4fa3d0a2b45e24e11f97cb0f9d40b8bd2
Fathima Hanna <fathimahannap123@gmail.com>
2026-02-19 16:06:23 +0530

    Add Google Fonts and update styles for improved aesthetics and responsiveness

--- web.js
+++ web.js
@@ -6,6 +6,23 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
+window.onscroll = () => {
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .project-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .abt-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .abt-content', { origin: 'right' });
+
 const form = document.getElementById('contact-form');
 const formStatus = document.getElementById('form-status');
 
